522140cff34efd296a1a8bcedc28f373a49bee05
Keiichiro Yamamoto <k.yanmar@gmail.com>
2015-05-20 22:39:46 +0900

    just add filter char2num, with straight letter output

--- js/app.js
+++ js/app.js
@@ -1,3 +1,18 @@
 (function() {
 
+angular.module('suhiColorApp', [])
+
+  .controller('suhiColorCtrl', function() {
+  })
+
+  .filter('char2num', function(){
+  	return function(input){
+	    input = input || '';
+	    var out = "";
+	    for (var i = 0; i < input.length; i++) {
+	      out = out + input.charAt(i);
+	    }
+  		return out;
+  	}
+  })
 })();
