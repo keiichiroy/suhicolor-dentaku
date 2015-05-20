0a0db3a2fdd69511106d11254c385ad93031a4f3
Keiichiro Yamamoto <k.yanmar@gmail.com>
2015-05-20 22:59:42 +0900

    convert alphabet to number from 1 to 9 (both capital and small)

--- js/app.js
+++ js/app.js
@@ -10,9 +10,16 @@ angular.module('suhiColorApp', [])
 	    input = input || '';
 	    var out = "";
 	    for (var i = 0; i < input.length; i++) {
-	      out = out + input.charCodeAt(i);
+	      var ascii = input.charCodeAt(i);
+	      if(ascii > 64 && ascii < 91){
+	        out = out + ((((ascii - 65) % 9)) + 1);
+	      }
+	      else if (ascii > 96 && ascii < 123){
+	        out = out + ((((ascii - 97) % 9)) + 1);
+	      }
 	    }
   		return out;
   	}
   })
+
 })();
